734b44662487005dfe71a6548d858d78b0a937a5
Jinkwan <jine2669@gmail.com>
2020-10-09 15:40:35 +0900

    update

---
+++ app/src/main/java/com/itm/lolMatching/auth/FirebaseAuth.kt
@@ -0,0 +1,19 @@
+package com.itm.lolMatching.auth
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class FirebaseAuth : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+
+}
+
+
+
---
+++ app/src/main/java/com/itm/lolMatching/auth/FirebaseUIActivity.kt
@@ -0,0 +1,13 @@
+package com.itm.lolMatching.auth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FirebaseUIActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity)
+    }
+}
