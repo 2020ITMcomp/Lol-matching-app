77615d8e6b8bf79d436cb2ed698cbe719d2dfcaf
Rurril <jine2669@gmail.com>
2020-10-11 22:25:19 +0900

    Update setting contentView
    set text from intent text

--- app/src/main/java/com/itm/lolMatching/SearchActivity.kt
+++ app/src/main/java/com/itm/lolMatching/SearchActivity.kt
@@ -2,7 +2,9 @@ package com.itm.lolMatching
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.itm.lolMatching.databinding.ActivitySearchBinding
+import kotlinx.android.synthetic.main.activity_search.view.*
 
 class SearchActivity : AppCompatActivity() {
 
@@ -12,10 +14,12 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         val view = binding.root
+        setContentView(view) // 바보같이, setContent를 안해서 아무것도 안나온 거였음...
 
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val message = intent.getStringExtra(EXTRA_MESSAGE).toString()
 
-        binding.sommernerName.text = message
+        view.sommernerName.setText(message)
 
+//        Log.d("123", message)
     }
 }
