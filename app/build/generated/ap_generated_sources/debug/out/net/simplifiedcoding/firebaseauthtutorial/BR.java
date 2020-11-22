a4bc3c2ea2695337aeed0ad77ed072d4a2e0350f
Jinkwan <jine2669@gmail.com>
2020-11-22 19:15:38 +0900

    fixed untracked files

--- app/build/generated/ap_generated_sources/debug/out/net/simplifiedcoding/firebaseauthtutorial/BR.java
+++
@@ -1,5 +0,0 @@
-package net.simplifiedcoding.firebaseauthtutorial;
-
-public class BR {
-  public static final int _all = 0;
-}
--- app/build/generated/source/dataBinding/trigger/debug/net/simplifiedcoding/firebaseauthtutorial/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package net.simplifiedcoding.firebaseauthtutorial;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
