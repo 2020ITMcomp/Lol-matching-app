c312dc00392abe5c90c12297795be5ae33dda63f
Jinkwan <jine2669@gmail.com>
2020-11-26 18:07:31 +0900

    룸이 꽉차서 닫히면 못들어오도록 수정

--- app/src/main/java/net/simplifiedcoding/firebaseauthtutorial/utils/FireStoreUtil.kt
+++ app/src/main/java/net/simplifiedcoding/firebaseauthtutorial/utils/FireStoreUtil.kt
@@ -30,7 +30,9 @@ fun getRoomRef(roomId: String): DocumentReference {
 fun getMatchingRoomListRef(summonerLane: Long, partnerLane: Long): Task<QuerySnapshot> {
     return db.collection("rooms")
         .whereEqualTo("summonerLane", summonerLane)
-        .whereEqualTo("partnerLane", partnerLane).get()
+        .whereEqualTo("partnerLane", partnerLane)
+        .whereEqualTo("isClosed", false)
+        .get()
 }
 
 fun getUserNicknameRef(uid : String) : Task<DocumentSnapshot> {
